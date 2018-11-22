6c3866f412c6db7921f9d0b654318573c4e9abf3
Jens Lindhardt <jens@lindhardt-freelance.dk>
2018-11-22 11:41:02 +0100

    Module 3, struct, class and collections

---
+++ DrinksMachine/Drinks/Coffee.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinksMachine.Drinks
+{
+    public struct Coffee
+    {
+        private int strength;
+        public int Strength
+        {
+            get
+            {
+                return strength;
+            }
+            set
+            {
+                if( strength <=0 )
+                {
+                    strength = value;
+                }
+            }
+        }
+
+        private string bean;
+        public string Bean
+        {
+            get
+            {
+                return bean;
+            }
+            set
+            {
+                if( string.IsNullOrEmpty( bean))
+                {
+                    bean = value;
+                }
+            }
+        }
+
+        private string countryOfOrigin;
+        public string CountryOfOrigin
+        {
+            get
+            {
+                return countryOfOrigin;
+            }
+            set
+            {
+                if( string.IsNullOrEmpty(countryOfOrigin) )
+                {
+                    countryOfOrigin = value;
+                }
+            }
+        }
+
+        public int Temperature { get; set; }
+
+        public Coffee(int strength, string bean, string countryOfOrigin)
+        {
+            this.strength = strength;
+            this.bean = bean;
+            this.countryOfOrigin = countryOfOrigin;
+            this.Temperature = 0;
+        }
+    }
+}
--- DrinksMachine/Program.cs
+++ DrinksMachine/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,42 @@ namespace DrinksMachine
     {
         static void Main(string[] args)
         {
+            Hashtable prices = new Hashtable();
+
+            prices.Add("Café au Lait", 1.99M);
+            prices.Add("Café Americano", 1.89M);
+            prices.Add("Café Mocha", 2.99M);
+            prices.Add("Cappuccino", 2.49M);
+            prices.Add("Espresso", 1.49M);
+            prices.Add("Espresso Romano", 1.59M);
+            prices.Add("English Tea", 1.99M);
+            prices.Add("Juice", 1.99M);
+
+
+
+            Drinks.Coffee coffee = new Drinks.Coffee(0, "Arabica", "Kenya");
+
+            Console.WriteLine("Country of orgin: {0}", coffee.CountryOfOrigin);
+            Console.WriteLine("Bean:             {0}", coffee.Bean);
+            Console.WriteLine("Strength:         {0}", coffee.Strength);
+
+            Console.ReadKey();
+
+            Drinks.Coffee coffee1 = new Drinks.Coffee()
+            {
+                Strength = 3,
+                Bean = "Ingen",
+                CountryOfOrigin = "Kina",
+                Temperature = 20
+            };
+
+            coffee.Strength = 7;
+
+            Console.WriteLine("Country of orgin: {0}", coffee.CountryOfOrigin);
+            Console.WriteLine("Bean:             {0}", coffee.Bean);
+            Console.WriteLine("Strength:         {0}", coffee.Strength);
+
+            Console.ReadKey();
         }
     }
 }
